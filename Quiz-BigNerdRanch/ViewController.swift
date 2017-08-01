16f9252e2c0fd4c5f9f8845ff1c76f5673b762e7
Sujanth <sujanth1987@gmail.com>
2017-08-02 08:31:41 +1000

    Added data model

--- Quiz-BigNerdRanch/ViewController.swift
+++ Quiz-BigNerdRanch/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
